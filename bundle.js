0865bd99bd75f90c5137ed4a14977b91aa490f7c
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2023-10-22 20:00:53 +0300

    Build home section

--- bundle.js
+++ bundle.js
@@ -2,6 +2,38 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _assets_pizza_top_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./assets/pizza-top.png */ "./src/assets/pizza-top.png");
+
+var renderHome = function renderHome() {
+  var homeSection = document.createElement("section");
+  homeSection.classList.add("home");
+  var heading = document.createElement("h1");
+  heading.classList.add("heading");
+  heading.textContent = "LA CARAVANA";
+  var paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph");
+  paragraph.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.\n        Vestibulum ut felis et lorem porta congue vitae nec turpis. Aliquam quis leo at nunc tempor faucibus et ac massa.";
+  var image = document.createElement("img");
+  image.src = _assets_pizza_top_png__WEBPACK_IMPORTED_MODULE_0__;
+  homeSection.appendChild(heading);
+  homeSection.appendChild(paragraph);
+  homeSection.appendChild(image);
+  return homeSection;
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderHome);
+
+/***/ }),
+
 /***/ "./src/website.js":
 /*!************************!*\
   !*** ./src/website.js ***!
@@ -12,7 +44,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-function renderHeader() {
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ "./src/home.js");
+
+var renderHeader = function renderHeader() {
   var header = document.createElement("header");
   header.classList.add("header");
   var restaurantLogo = document.createElement("a");
@@ -22,8 +56,8 @@ function renderHeader() {
   header.appendChild(restaurantLogo);
   header.appendChild(renderNavigation());
   return header;
-}
-function renderNavigation() {
+};
+var renderNavigation = function renderNavigation() {
   var navigation = document.createElement("nav");
   navigation.classList.add("nav");
   var homeLink = document.createElement("a");
@@ -47,11 +81,18 @@ function renderNavigation() {
   navigation.appendChild(menuLink);
   navigation.appendChild(contactLink);
   return navigation;
-}
-function renderWebsite() {
-  var container = document.querySelector(".container");
-  container.appendChild(renderHeader());
-}
+};
+var renderMain = function renderMain() {
+  var mainSection = document.createElement("main");
+  mainSection.classList.add("main");
+  mainSection.appendChild((0,_home__WEBPACK_IMPORTED_MODULE_0__["default"])());
+  return mainSection;
+};
+var renderWebsite = function renderWebsite() {
+  var body = document.querySelector("body");
+  body.appendChild(renderHeader());
+  body.appendChild(renderMain());
+};
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderWebsite);
 
 /***/ }),
@@ -70,11 +111,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
 /* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/getUrl.js */ "./node_modules/css-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);
 // Imports
 
 
+
+var ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/background.jpeg */ "./src/assets/background.jpeg"), __webpack_require__.b);
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 ___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Ruda&display=swap);"]);
+var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);
 // Module
 ___CSS_LOADER_EXPORT___.push([module.id, `/* General Styles */
 
@@ -90,7 +136,10 @@ html {
 }
 
 body {
-    min-height: 100vh;
+    height: 100vh;
+    background-color: #000;
+    display: flex;
+    flex-direction: column;
 }
 
 ul {
@@ -109,17 +158,13 @@ button {
     font: inherit;
 }
 
-.container {
-    background-color: #000;
-}
-
 /* Header */
 
 .header {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 3rem;
+    padding: 2rem;
 }
 
 .header__logo {
@@ -154,7 +199,55 @@ button {
 .nav__link:active {
     border-bottom: 1px solid currentColor;
 }
-`, "",{"version":3,"sources":["webpack://./src/styles/style.css"],"names":[],"mappings":"AAAA,mBAAmB;;AAInB;IACI,UAAU;IACV,SAAS;IACT,sBAAsB;AAC1B;;AAEA;IACI,gBAAgB;IAChB,uBAAuB;AAC3B;;AAEA;IACI,iBAAiB;AACrB;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,qBAAqB;IACrB,cAAc;AAClB;;AAEA;IACI,eAAe;IACf,gBAAgB;IAChB,YAAY;IACZ,aAAa;AACjB;;AAEA;IACI,sBAAsB;AAC1B;;AAEA,WAAW;;AAEX;IACI,aAAa;IACb,mBAAmB;IACnB,8BAA8B;IAC9B,aAAa;AACjB;;AAEA;IACI,WAAW;IACX,kBAAkB;IAClB,iBAAiB;IACjB,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,sBAAsB;AAC1B;;AAEA;IACI,aAAa;IACb,mBAAmB;IACnB,SAAS;AACb;;AAEA;;IAEI,WAAW;IACX,kBAAkB;IAClB,iBAAiB;IACjB,eAAe;IACf,kBAAkB;IAClB,gBAAgB;IAChB,oCAAoC;IACpC,oBAAoB;AACxB;;AAEA;;IAEI,qCAAqC;AACzC","sourcesContent":["/* General Styles */\n\n@import url(\"https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Ruda&display=swap\");\n\n* {\n    padding: 0;\n    margin: 0;\n    box-sizing: border-box;\n}\n\nhtml {\n    font-size: 62.5%;\n    scroll-behavior: smooth;\n}\n\nbody {\n    min-height: 100vh;\n}\n\nul {\n    list-style: none;\n}\n\na {\n    text-decoration: none;\n    color: inherit;\n}\n\nbutton {\n    cursor: pointer;\n    background: none;\n    border: none;\n    font: inherit;\n}\n\n.container {\n    background-color: #000;\n}\n\n/* Header */\n\n.header {\n    display: flex;\n    align-items: center;\n    justify-content: space-between;\n    padding: 3rem;\n}\n\n.header__logo {\n    color: #fff;\n    text-align: center;\n    font-family: Ruda;\n    font-size: 2.4rem;\n    font-style: normal;\n    font-weight: 400;\n    letter-spacing: 1.44px;\n}\n\n.nav {\n    display: flex;\n    align-items: center;\n    gap: 3rem;\n}\n\n.nav__link:link,\n.nav__link:visited {\n    color: #fff;\n    text-align: center;\n    font-family: Ruda;\n    font-size: 20px;\n    font-style: normal;\n    font-weight: 400;\n    border-bottom: 1px solid transparent;\n    transition: all 0.3s;\n}\n\n.nav__link:hover,\n.nav__link:active {\n    border-bottom: 1px solid currentColor;\n}\n"],"sourceRoot":""}]);
+
+/* Main */
+.main {
+    flex-grow: 1;
+}
+
+/* Home Section */
+
+.home {
+    height: 100%;
+    background: linear-gradient(
+            180deg,
+            rgba(0, 0, 0, 0) 0%,
+            rgba(39, 39, 39, 0.1) 0.01%,
+            rgba(0, 0, 0, 0.13) 13.02%,
+            #000 100%
+        ),
+        url(${___CSS_LOADER_URL_REPLACEMENT_0___});
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.heading {
+    color: #fff;
+    text-align: center;
+    font-family: Quattrocento;
+    font-size: 7.2rem;
+    font-style: normal;
+    font-weight: 700;
+    border-bottom: 4px solid #f2c94c;
+    padding-top: 15rem;
+}
+
+.paragraph {
+    color: #fff;
+    text-align: center;
+    font-family: Ruda;
+    font-size: 2.5rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 40px;
+    letter-spacing: 2.25px;
+    max-width: 74.5rem;
+    padding-top: 1.5rem;
+}
+`, "",{"version":3,"sources":["webpack://./src/styles/style.css"],"names":[],"mappings":"AAAA,mBAAmB;;AAInB;IACI,UAAU;IACV,SAAS;IACT,sBAAsB;AAC1B;;AAEA;IACI,gBAAgB;IAChB,uBAAuB;AAC3B;;AAEA;IACI,aAAa;IACb,sBAAsB;IACtB,aAAa;IACb,sBAAsB;AAC1B;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,qBAAqB;IACrB,cAAc;AAClB;;AAEA;IACI,eAAe;IACf,gBAAgB;IAChB,YAAY;IACZ,aAAa;AACjB;;AAEA,WAAW;;AAEX;IACI,aAAa;IACb,mBAAmB;IACnB,8BAA8B;IAC9B,aAAa;AACjB;;AAEA;IACI,WAAW;IACX,kBAAkB;IAClB,iBAAiB;IACjB,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,sBAAsB;AAC1B;;AAEA;IACI,aAAa;IACb,mBAAmB;IACnB,SAAS;AACb;;AAEA;;IAEI,WAAW;IACX,kBAAkB;IAClB,iBAAiB;IACjB,eAAe;IACf,kBAAkB;IAClB,gBAAgB;IAChB,oCAAoC;IACpC,oBAAoB;AACxB;;AAEA;;IAEI,qCAAqC;AACzC;;AAEA,SAAS;AACT;IACI,YAAY;AAChB;;AAEA,iBAAiB;;AAEjB;IACI,YAAY;IACZ;;;;;;;+CAOoC;;IAEpC,aAAa;IACb,sBAAsB;IACtB,mBAAmB;IACnB,8BAA8B;AAClC;;AAEA;IACI,WAAW;IACX,kBAAkB;IAClB,yBAAyB;IACzB,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,gCAAgC;IAChC,kBAAkB;AACtB;;AAEA;IACI,WAAW;IACX,kBAAkB;IAClB,iBAAiB;IACjB,iBAAiB;IACjB,kBAAkB;IAClB,gBAAgB;IAChB,iBAAiB;IACjB,sBAAsB;IACtB,kBAAkB;IAClB,mBAAmB;AACvB","sourcesContent":["/* General Styles */\n\n@import url(\"https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Ruda&display=swap\");\n\n* {\n    padding: 0;\n    margin: 0;\n    box-sizing: border-box;\n}\n\nhtml {\n    font-size: 62.5%;\n    scroll-behavior: smooth;\n}\n\nbody {\n    height: 100vh;\n    background-color: #000;\n    display: flex;\n    flex-direction: column;\n}\n\nul {\n    list-style: none;\n}\n\na {\n    text-decoration: none;\n    color: inherit;\n}\n\nbutton {\n    cursor: pointer;\n    background: none;\n    border: none;\n    font: inherit;\n}\n\n/* Header */\n\n.header {\n    display: flex;\n    align-items: center;\n    justify-content: space-between;\n    padding: 2rem;\n}\n\n.header__logo {\n    color: #fff;\n    text-align: center;\n    font-family: Ruda;\n    font-size: 2.4rem;\n    font-style: normal;\n    font-weight: 400;\n    letter-spacing: 1.44px;\n}\n\n.nav {\n    display: flex;\n    align-items: center;\n    gap: 3rem;\n}\n\n.nav__link:link,\n.nav__link:visited {\n    color: #fff;\n    text-align: center;\n    font-family: Ruda;\n    font-size: 20px;\n    font-style: normal;\n    font-weight: 400;\n    border-bottom: 1px solid transparent;\n    transition: all 0.3s;\n}\n\n.nav__link:hover,\n.nav__link:active {\n    border-bottom: 1px solid currentColor;\n}\n\n/* Main */\n.main {\n    flex-grow: 1;\n}\n\n/* Home Section */\n\n.home {\n    height: 100%;\n    background: linear-gradient(\n            180deg,\n            rgba(0, 0, 0, 0) 0%,\n            rgba(39, 39, 39, 0.1) 0.01%,\n            rgba(0, 0, 0, 0.13) 13.02%,\n            #000 100%\n        ),\n        url(\"../assets/background.jpeg\");\n\n    display: flex;\n    flex-direction: column;\n    align-items: center;\n    justify-content: space-between;\n}\n\n.heading {\n    color: #fff;\n    text-align: center;\n    font-family: Quattrocento;\n    font-size: 7.2rem;\n    font-style: normal;\n    font-weight: 700;\n    border-bottom: 4px solid #f2c94c;\n    padding-top: 15rem;\n}\n\n.paragraph {\n    color: #fff;\n    text-align: center;\n    font-family: Ruda;\n    font-size: 2.5rem;\n    font-style: normal;\n    font-weight: 400;\n    line-height: 40px;\n    letter-spacing: 2.25px;\n    max-width: 74.5rem;\n    padding-top: 1.5rem;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -251,6 +344,41 @@ module.exports = function (cssWithMappingToString) {
     }
   };
   return list;
+};
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+/*!********************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
+  \********************************************************/
+/***/ ((module) => {
+
+
+
+module.exports = function (url, options) {
+  if (!options) {
+    options = {};
+  }
+  if (!url) {
+    return url;
+  }
+  url = String(url.__esModule ? url.default : url);
+
+  // If url is already wrapped in quotes, remove them
+  if (/^['"].*['"]$/.test(url)) {
+    url = url.slice(1, -1);
+  }
+  if (options.hash) {
+    url += options.hash;
+  }
+
+  // Should url be wrapped?
+  // See https://drafts.csswg.org/css-values-3/#urls
+  if (/["'() \t\n]|(%20)/.test(url) || options.needQuotes) {
+    return "\"".concat(url.replace(/"/g, '\\"').replace(/\n/g, "\\n"), "\"");
+  }
+  return url;
 };
 
 /***/ }),
@@ -599,6 +727,26 @@ function styleTagTransform(css, styleElement) {
 }
 module.exports = styleTagTransform;
 
+/***/ }),
+
+/***/ "./src/assets/background.jpeg":
+/*!************************************!*\
+  !*** ./src/assets/background.jpeg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "background.jpeg";
+
+/***/ }),
+
+/***/ "./src/assets/pizza-top.png":
+/*!**********************************!*\
+  !*** ./src/assets/pizza-top.png ***!
+  \**********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "pizza-top.png";
+
 /***/ })
 
 /******/ 	});
@@ -627,6 +775,9 @@ module.exports = styleTagTransform;
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -652,6 +803,18 @@ module.exports = styleTagTransform;
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -668,6 +831,55 @@ module.exports = styleTagTransform;
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"bundle": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/nonce */
 /******/ 	(() => {
 /******/ 		__webpack_require__.nc = undefined;
@@ -682,10 +894,10 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/style.css */ "./src/styles/style.css");
-/* harmony import */ var _website_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./website.js */ "./src/website.js");
+/* harmony import */ var _website__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./website */ "./src/website.js");
 
 
-(0,_website_js__WEBPACK_IMPORTED_MODULE_1__["default"])();
+(0,_website__WEBPACK_IMPORTED_MODULE_1__["default"])();
 })();
 
 /******/ })()
